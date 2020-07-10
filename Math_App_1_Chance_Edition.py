99765c8c80effa08452293359553848fe7d132fe
charl <charlie.li711935@gmail.com>
2020-07-10 16:40:34 -0400

    Matched question_id of the history and wrong_questions table. Also added a delete corrected questions feature

--- Math_App_1_Chance_Edition.py
+++ Math_App_1_Chance_Edition.py
@@ -29,6 +29,7 @@ class Operation:
         self.oper_func = oper_func
 
     def function(self):
+        global wrong_question_id
         self.result = self.oper_func(self.n1, self.n2)
         python_question = str(self.n1) + self.func_symbol + str(self.n2) + " = "
         user_answer = input(python_question)
@@ -47,8 +48,13 @@ class Operation:
             insert_into_history = "INSERT INTO history (question, equation, is_answer_correct, date_completed, time_completed) VALUES (%s, %s, %s, %s, %s)"
             mycursor.execute(insert_into_history, complete_insert_into_history)
             # mycursor.execute("INSERT INTO wrong_questions SELECT question_id FROM history")
-            complete_insert_into_wrong_questions = (python_question, self.n1, self.func_symbol, self.n2)
-            insert_into_wrong_questions = "INSERT INTO wrong_questions (question, number_1, operator, number_2) VALUES (%s, %s, %s, %s)"
+            mycursor.execute("SELECT MAX(question_id) FROM history")
+            for wrong_question_id_tuple in mycursor:
+                for individual_wrong_question_id in wrong_question_id_tuple:
+                    wrong_question_id = individual_wrong_question_id
+            complete_insert_into_wrong_questions = (wrong_question_id, python_question, self.n1, self.func_symbol, self.n2)
+            insert_into_wrong_questions = "INSERT INTO wrong_questions (question_id, question, number_1, operator, number_2) VALUES (%s, %s, %s, %s, %s)"
+            # mycursor.execute("INSERT INTO wrong_questions (question_id) SELECT MAX(question_id) FROM history")
             mycursor.execute(insert_into_wrong_questions, complete_insert_into_wrong_questions)
             mydb.commit()
 
@@ -77,30 +83,49 @@ overall_score = score_frame.format(Operation.score, number_of_questions)
 
 print(overall_score)
 
-if Operation.score - number_of_questions != 0:
+mycursor.execute("SELECT COUNT(*) FROM wrong_questions")
+
+global amount_of_rows
+for total_counts in mycursor:
+    for individual_total_count in total_counts:
+        amount_of_rows = individual_total_count
+
+if amount_of_rows != 0:
     redo = input("Would you like to redo the wrong questions? (Yes/No): ")
 
     while True:
         if redo == "Yes" or redo == "No":
             break
         else:
+
             redo = input("Please answer Yes or No: ")
 
     if redo == "No":
         quit()
+else:
+    quit()
+
+list_of_wrong_questions = []
 
 mycursor.execute("SELECT question FROM wrong_questions")
-for question in mycursor:
-    print(question)
+for questions in mycursor:
+    for individual_question in questions:
+        list_of_wrong_questions.append(individual_question)
 
-list_of_question_ids = []
+print()
+print("Wrong Questions: ")
+for each_question in list_of_wrong_questions:
+    print(each_question)
+
+list_of_wrong_question_ids = []
 
 mycursor.execute("SELECT question_id FROM wrong_questions")
 for question_ids in mycursor:
     for individual_number_1 in question_ids:
-        list_of_question_ids.append(individual_number_1)
+        list_of_wrong_question_ids.append(individual_number_1)
 
-print(list_of_question_ids)
+print()
+print("Question IDs: " + str(list_of_wrong_question_ids))
 
 list_of_number_1s = []
 
@@ -109,7 +134,8 @@ for number_1s in mycursor:
     for individual_number_1 in number_1s:
         list_of_number_1s.append(individual_number_1)
 
-print(list_of_number_1s)
+print()
+print("Number 1s: " + str(list_of_number_1s))
 
 list_of_operators = []
 
@@ -118,7 +144,8 @@ for operators in mycursor:
     for individual_operator in operators:
         list_of_operators.append(individual_operator)
 
-print(list_of_operators)
+print()
+print("Operators: " + str(list_of_operators))
 
 list_of_number_2s = []
 
@@ -127,20 +154,22 @@ for number_2s in mycursor:
     for individual_number_2 in number_2s:
         list_of_number_2s.append(individual_number_2)
 
-print(list_of_number_2s)
+print()
+print("Number 2s: " + str(list_of_number_2s))
+print()
 
 number_of_wrong_questions = len(list_of_number_1s)
 question_number = 0
 
 def redo_wrong_questions(question_number):
+    print("Beginning your retrial: ")
     redo_result = operations[list_of_operators[question_number]](list_of_number_1s[question_number], list_of_number_2s[question_number])
     python_redo_question = str(list_of_number_1s[question_number]) + str(list_of_operators[question_number]) + str(list_of_number_2s[question_number]) + " = "
     user_redo_answer = input(python_redo_question)
     if str(user_redo_answer) == str(redo_result):
         print("You are correct!")
-        python_equation = str(python_redo_question) + str(user_redo_answer) + "    ✓"
-        complete_delete_wrong_question_from_history = list_of_question_ids[question_number]
-        delete_wrong_question_from_history = "DELETE FROM history WHERE (question_id = %s)"
+        complete_delete_wrong_question_from_history = list_of_wrong_question_ids[question_number]
+        delete_wrong_question_from_history = "DELETE FROM wrong_questions WHERE (question_id = %s)"
         mycursor.execute(delete_wrong_question_from_history, (complete_delete_wrong_question_from_history,))
         mydb.commit()
         # complete_delete_wrong_question_from_wrong_questions = list_of_question_ids[question_number]
